9a386f26af6e2d815f9d7d4c0e20229d5d9e67b5
GownerCode <flo.klar@googlemail.com>
2022-05-26 18:59:12 +0200

    d

--- code/first_test.py
+++ code/first_test.py
@@ -2,10 +2,37 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Device
 from gpiozero import Servo
 from time import sleep
+from pynput import keyboard
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False  # stop listener
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys
+    if k in ['up', 'down']:  # keys of interest
+        # self.keys.append(k)  # store it in global-like variable
+        if k == "up":
+            stepUp()
+        elif k == "down":
+            stepDown()
+        return False  # stop listener; remove this if want more keys
 
 Device.pin_factory = PiGPIOFactory()
 
 servo = Servo(25)
 
-servo.max()
-sleep(10)
+servo.mid()
+sleep(1)
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()  # start to listen on a separate thread
+listener.join()
+
+def stepUp():
+    servo.value -= 0.01
+
+def stepDown():
+    servo.value += 0.01
+
