4ed7e9f9f66a5224f68eb4367230797c43077069
GownerCode <flo.klar@googlemail.com>
2022-05-26 18:45:11 +0200

    d

--- code/first_test.py
+++ code/first_test.py
@@ -1,7 +1,11 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Device
 from gpiozero import Servo
 from time import sleep
 
+Device.pin_factory = PiGPIOFactory()
+
 servo = Servo(25)
 
-servo.min()
+servo.max()
 sleep(10)
