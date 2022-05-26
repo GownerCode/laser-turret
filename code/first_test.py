7d5d83451a0a5224379337ede38a460cc7521e53
GownerCode <flo.klar@googlemail.com>
2022-05-26 19:25:30 +0200

    d

--- code/first_test.py
+++ code/first_test.py
@@ -19,7 +19,7 @@ def onKeyPress(event):
 
 root = tk.Tk()
 root.geometry('300x200')
-text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+text = tk.Text(root, background='black', foreground='white')
 text.pack()
 root.bind('<KeyPress>', onKeyPress)
 root.mainloop()
